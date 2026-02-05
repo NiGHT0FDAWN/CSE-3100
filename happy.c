e806f428626aeaaccd6115588edabf68e725c2b3
NiGHT0FDAWN <131709204+NiGHT0FDAWN@users.noreply.github.com>
2026-02-05 02:27:49 +0000

    :)

--- happy.c
+++ happy.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+int step(int m){
+    int pwr;
+    int tot = 0;
+    while(m>0){
+        pwr = m%10;
+        m/=10;
+        tot += pwr*pwr;
+    }
+    return tot;
+}
 
 int main()
 {
@@ -9,16 +19,11 @@ int main()
 	scanf("%d", &n);
 
 	int m = n;
-	//TODO
-	//add code below
-	
-
-
-
-
-
-
-	if(n==1) printf("%d is a happy number.\n", m);
+    while(n!=4 && n!=1){
+        n = step(n);
+        printf("%d\n",n);
+    }
+    if(n==1) printf("%d is a happy number.\n", m);
 	else printf("%d is NOT a happy number.\n", m);
 	return 0;
 }
