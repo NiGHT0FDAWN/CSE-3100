16fb26a35ced4e8195922a2b6f05f96dd42bb3a8
NiGHT0FDAWN <131709204+NiGHT0FDAWN@users.noreply.github.com>
2026-02-27 19:05:33 +0000

    :)

--- place.c
+++ place.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
+int comp(const void *a, const void *b) {
+        char *sa = *(const char **)a;
+        char *sb = *(const char **)b;
+        int ca = 0, cb = 0;
+        for (char *p = sa; *p; p++)
+                if (islower((char)*p)) {
+                    ca++;
+                }
+        for (char *p = sb; *p; p++)
+                if (islower((char)*p)) {
+                    cb++;
+                }
+        return ca - cb;
+}
 
 void print_elements(char **elems, int count) {
-  for (int i = 0; i < count; i++) {
-    printf("%s\n", elems[i]);
-  }
+    for (int i = 0; i < count; i++) {
+        printf("%s\n", elems[i]);
+    }
 }
 
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    perror("Usage : ./place <strings>");
-    exit(1);
-  }
+    if (argc < 2) {
+        perror("Usage : ./place <strings>");
+        exit(1);
+    }
+    qsort(&argv[1], argc - 1, sizeof(char *), comp);
+    print_elements(&argv[1], argc - 1);
 
-  print_elements(&argv[1], argc - 1);
-
-  return 0;
+    return 0;
 }
--- survival.c
+++ survival.c
@@ -2,28 +2,30 @@
 #include <stdlib.h>
 
 typedef struct particle {
-  int mass;
-  struct particle *next;
+    int mass;
+    struct particle *next;
 } ParticleSet;
 
-void addToSet(ParticleSet **p, int v) {}
+void addToSet(ParticleSet **p, int v) {
+    v->next = p[0];
+}
 
-int removeFromSet(ParticleSet **p) {}
-
+int removeFromSet(ParticleSet **p) {
+}
 int isSetEmpty(ParticleSet *p) {}
 
 int topOfTheSet(ParticleSet *p) {}
 
 void printSet(ParticleSet *p) {
-  if (isSetEmpty(p)) {
-    printf("Empty\n");
-    return;
-  }
-  while (p != NULL) {
-    printf("%d ", p->mass);
-    p = p->next;
-  }
-  printf("\n");
+    if (isSetEmpty(p)) {
+        printf("Empty\n");
+        return;
+    }
+    while (p != NULL) {
+        printf("%d ", p->mass);
+        p = p->next;
+    }
+    printf("\n");
 }
 
 void freeSet(ParticleSet **b) {}
@@ -31,20 +33,20 @@ void freeSet(ParticleSet **b) {}
 ParticleSet *fight(int *particles, int count) {}
 
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    fprintf(stderr, "Usage: ./survival <particle values>\n");
-    exit(1);
-  }
+    if (argc < 2) {
+        fprintf(stderr, "Usage: ./survival <particle values>\n");
+        exit(1);
+    }
 
-  int count = argc - 1;
-  int *particles = malloc(count * sizeof(int));
+    int count = argc - 1;
+    int *particles = malloc(count * sizeof(int));
 
-  for (int i = 0; i < count; i++) {
-    particles[i] = atoi(argv[i + 1]);
-  }
+    for (int i = 0; i < count; i++) {
+        particles[i] = atoi(argv[i + 1]);
+    }
 
-  ParticleSet *b = fight(particles, count);
-  printSet(b);
-  
-	return 0;
+    ParticleSet *b = fight(particles, count);
+    printSet(b);
+    
+    return 0;
 }
